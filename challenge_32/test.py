39b7c6434a785a919fee169be0db154d7f41349b
devnrj07 <niraj.mukta123@gmail.com>
2020-08-21 12:17:27 +0530

    added test_cases for 32

--- challenge_32/test.py
+++ challenge_32/test.py
@@ -1,66 +1,31 @@
-# The calculator app
+#Test cases for calculator app
+from main import add, subtract, multiply, division, exponent
+import pytest
 
-def add(a:float, b:float) -> float:
-    print('\n performing addition operation ')
-    return f'{a}+{b}={round(a+b,4)}'
-def subtract(a:float, b:float) -> float:
-    print('\n performing subtraction operation ')
-    return f'{a}-{b}=round(a-b,4)'
-def multiply(a:float, b:float) -> float:
-    print('\n performing multiplication operation ')
-    return f'{a}*{b} = {round(a*b,4)}'
-def division(a:float, b:float) -> float:            
-    print('\n performing division operation ')
-    if b == 0:
-        print('\n You cannot divide by zero.')
-        raise Exception('Error: DIV ERROR')
-    else:
-        return f'{a}/{b} = {round(a/b,4)}'
-def exponent(a:float, b:float) -> float:
-    print('\n Performing exponential operation')
-    return f'{a} ** {b} = {round(a**b,)}'
+@pytest.mark.parametrize('test_input_1,test_input_2, expected', [
+    (10,10, '10+10 = 20'),
+    (10,-10, '10+-10 = 0'),
+    (-10,-10, '-10+-10 = -20'),
+    ])
+def test_add(test_input_1, test_input_2, expected):
+    assert add(test_input_1, test_input_2) == expected
 
-def continue_app() ->bool:
-    response = input('\n Do you want to continue ? (y/n):').lower()
-    if response.startswith('y'):
-        return True
-    else:
-        return False    
+def test_subtract():
+    result = subtract(20,30)
+    expected = '20-30 = -10'
+    assert result == expected
 
-if __name__ == "__main__":
-    print('\n Welcome to the basic calculator app. ')
-    active=True
-    history = []
-    while active:
-        inputs = input('\n Enter the numbers comma separated : ').split(',')
-        operation = input('\n Enter the type of operation you want to perform :').lower()
-        arg_one = float(inputs[0])
-        arg_two = float(inputs[1])
-        if operation.startswith('a'):
-            result = add(arg_one,arg_two)
-            history.append(result)
-            active = continue_app()
-        elif operation.startswith('s'):
-            result = subtract(arg_one,arg_two)
-            history.append(result)
-            active=continue_app()
-        elif operation.startswith('m'):
-            result = multiply(arg_one,arg_two)
-            history.append(result)
-            active=continue_app()        
-        elif operation.startswith('d'):
-            result = division(arg_one,arg_two)
-            history.append(result)
-            active=continue_app()
-        elif operation.startswith('e'):
-            result = exponent(arg_one,arg_two)
-            history.append(result)
-            active=continue_app()
-        else:
-            print('\n Invalid Operations. Operation not supported.') 
-            history.append('Invalid Operation.')
-            active=continue_app()
-    print('Summary of your operations :')
-    for operation in  history:
-        print(f'\n {operation}')        
-    print('\n Thank you for playing with us.')
+def test_multiply():
+    result = multiply(-1.1, 4)
+    expected = '-1.1*4 = -4.4'
+    assert result == expected
+
+@pytest.mark.parametrize('input_1, input_2, expected', 
+[(10, 15, '10/15 = 0.6667'),
+ #pytest.param(10,0, marks=pytest.raises(Exception, match='Error: DIV ERROR'))   
+])
+def test_division(input_1,input_2,expected):
+    assert division(input_1,input_2) == expected
+
+def test_exponent():
+    assert exponent(3,2) == '3 ** 2 = 9'
